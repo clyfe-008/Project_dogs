7a5f0410fce3217e866dc60670f30869d6bacd19
barakatimothy <timothybaraka39@gmail.com>
2023-10-04 23:50:04 +0300

    Feat: made major changes to the review

--- client/dogs-airbnb/src/components/Review.js
+++ client/dogs-airbnb/src/components/Review.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect}from 'react';
 
 const Review = () => {
+    const [dogReview, setDogReview ]= useState(null)
+     const [loading, setLoading] = useState(true)
+
+     useEffect(() =>{
+      const FetchReview = async () => {
+        try{
+          setLoading(true);
+         
+          const response = await fetch ('')
+           if(!response.ok){
+            throw new Error('Failed to fetch data!')
+        }
+        const data = await response.json();
+        setDogReview(dogReview);
+        }
+        catch (error){
+          console.error('Error fetching data:', error)
+        }
+        finally{
+          setLoading(false);
+        }
+       }
+       FetchReview();
+     },[])
+  
   return (
-      <>
-        <div className="review-container">
-            <h2 className="review-title">Review 1</h2>
-            <p className="review-content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse
-              lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.
-            </p>
+    <div>
+      {loading && <p>Loading...</p>}
+      {dogReview &&(
+        <div>
+          <h2>Dog Details</h2>
+          <p>Name: {dogReview.userId}</p>
+          <p>Breed: {dogReview.dogHouseId}</p>
+          <p>Rating: {dogReview.content}</p>
         </div>
-        <div className="review-container">
-            <h2 className="review-title">Review 2</h2>
-            <p className="review-content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse
-              lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.
-            </p>
-      </div>
-    </>
+        )}
+    </div>
 );
 };
 export default Review;
---
+++ client/dogs-airbnb/src/components/SidebarData.js
@@ -0,0 +1,38 @@
+import React from "react";
+import * as  FaIcons  from "react-icons/fa";
+import * as AiIcons  from  'react-icons/ai'
+import * as Iocons  from  'react-icons/io'
+import 'SideBar.css' from 'styles'
+
+export const SidebarData = [
+    {
+        title:'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome></AiIcons.AiFillHome>,
+        cName: 'nav-text'
+    },
+    {
+        title:'Maps',
+        path: '/',
+        icon: <AiIcons.AiFillHome></AiIcons.AiFillHome>,
+        cName: 'nav-text'
+    },
+    {
+        title:'Contact_us ',
+        path: '/',
+        icon: <AiIcons.AiFillHome></AiIcons.AiFillHome>,
+        cName: 'nav-text'
+    },
+    {
+        title:'Team',
+        path: '/',
+        icon: <AiIcons.AiFillHome></AiIcons.AiFillHome>,
+        cName: 'nav-text'
+    },
+    {
+        title:'Developer Support',
+        path: '/',
+        icon: <AiIcons.AiFillHome></AiIcons.AiFillHome>,
+        cName: 'nav-text'
+    }
+]

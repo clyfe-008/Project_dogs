aa9d32fa964be0cdf642b5dd63dc272297582c25
Ruan <cliff.kamau@student.moringaschool.com>
2023-10-04 21:26:43 +0300

    seed data updated

--- server/seed.py
+++ server/seed.py
@@ -1,33 +1,38 @@
-from app import app, db  # Update these import statements based on your project structure
+from app import app, db
 from model import User, DogHouse, Review
+from faker import Faker
+import random
+
+fake = Faker()
 
 def seed_database():
     with app.app_context():
-        # Create the database schema
+        
         db.create_all()
 
         # Create and commit users
-        user1 = User(username='Ngoro', email='ngoro@gmail.com', password='1957')
-        user2 = User(username='Becky', email='rebe@gmail.com', password='Ndegwa7')
-        user3 = User(username='Ngima', email='loise@gmail.com', password='mweri5')
-        user4 = User(username='Muthoni', email='mary@gmail.com', password='kamau20')
+        users = []
+        for _ in range(10):
+            username = fake.user_name()
+            email = fake.email()
+            password = fake.password()
+            user = User(username=username, email=email, password=password)
+            users.append(user)
 
-        db.session.add(user1)
-        db.session.add(user2)
-        db.session.add(user3)
-        db.session.add(user4)
+        db.session.add_all(users)
         db.session.commit()
 
-        # Create and commit dog houses
-        doghouse1 = DogHouse(name='Oloolua', address='123 Main St', average_rating=4.5, user_id=user1.id)
-        doghouse2 = DogHouse(name='Bul', address='456 Elm St', average_rating=3.8, user_id=user2.id)
-        doghouse3 = DogHouse(name='Green Acres', address='789 Oak St', average_rating=4.0, user_id=user3.id)
-        doghouse4 = DogHouse(name='Sunny Side', address='101 Pine St', average_rating=4.2, user_id=user4.id)
+        # Create and commit dog houses with random ratings
+        dog_houses = []
+        for _ in range(10):
+            name = fake.company()
+            address = fake.address()
+            average_rating = round(random.uniform(3.0, 5.0), 1)
+            user_id = random.choice(users).id
+            dog_house = DogHouse(name=name, address=address, average_rating=average_rating, user_id=user_id)
+            dog_houses.append(dog_house)
 
-        db.session.add(doghouse1)
-        db.session.add(doghouse2)
-        db.session.add(doghouse3)
-        db.session.add(doghouse4)
+        db.session.add_all(dog_houses)
         db.session.commit()
 
 if __name__ == '__main__':
